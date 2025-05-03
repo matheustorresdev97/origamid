1e5e224d84e99ca6cf14ffb3e7e8b099198f70f3
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-03 20:23:28 -0300

    feat: scroll suave

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -38,6 +38,34 @@ function initAccordion() {
   }
 }
 
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+    });
+
+    // forma alternativa
+    // window.scrollTo({
+    //   top: section.offsetTop,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
+
 initTabNav();
 
 initAccordion();
+
+initScrollSuave();
