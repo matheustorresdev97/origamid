2e88afff8aa432a77c3561567bad8898be270196
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-08 19:44:47 -0300

    feat: fetch bitcoin

--- animais-fantasticos/js/modules/fetch-animais.js
+++ animais-fantasticos/js/modules/fetch-animais.js
@@ -1,29 +1,33 @@
 import { initAnimaNumeros } from "./anima-numeros.js";
 
 export function initFetchAnimais() {
-  async function fetchAnimais(url) {
-    const animaisResponse = await fetch(url);
-    const animaisJson = await animaisResponse.json();
-    const numerosGrid = document.querySelector(".numeros-grid");
+    async function fetchAnimais(url) {
+        try {
+            const animaisResponse = await fetch(url);
+            const animaisJson = await animaisResponse.json();
+            const numerosGrid = document.querySelector(".numeros-grid");
 
-    animaisJson.forEach((animal) => {
-      const divAnimal = createAnimal(animal);
-      numerosGrid.appendChild(divAnimal);
-    });
+            animaisJson.forEach((animal) => {
+                const divAnimal = createAnimal(animal);
+                numerosGrid.appendChild(divAnimal);
+            });
 
-    initAnimaNumeros();
-  }
+            initAnimaNumeros();
+        } catch (erro) {
+            console.log(erro);
+        }
+    }
 
-  function createAnimal(animal) {
-    const div = document.createElement("div");
-    div.classList.add("numero-animal");
-    div.innerHTML = `
+    function createAnimal(animal) {
+        const div = document.createElement("div");
+        div.classList.add("numero-animal");
+        div.innerHTML = `
   <h3>${animal.specie}</h3>
   <span data-numero>${animal.total}</span>
   `;
 
-    return div;
-  }
+        return div;
+    }
 
-  fetchAnimais("./animais-api.json");
+    fetchAnimais("./animais-api.json");
 }
