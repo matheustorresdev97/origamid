a45577097c6390870bf7c79bd64fd9fa1554cb7e
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-08 18:45:38 -0300

    feat: separando em modulos

---
+++ animais-fantasticos/js/script.js
@@ -0,0 +1,9 @@
+import { initAccordion } from "./modules/accordion.js";
+import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
+import { initScrollSuave } from "./modules/scroll-suave.js";
+import { initTabNav } from "./modules/tabnav.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
