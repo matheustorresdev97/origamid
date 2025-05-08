c2b274f88c677009222ad7806e88e7c38aebc908
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-08 19:10:06 -0300

    feat: anima numeros

--- animais-fantasticos/js/script.js
+++ animais-fantasticos/js/script.js
@@ -3,9 +3,10 @@ import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
 import { initAccordion } from "./modules/accordion.js";
 import { initTabNav } from "./modules/tabnav.js";
 import { initModal } from "./modules/modal.js";
-import { initTooltip } from "./modules/tolltip.js";
+import { initTooltip } from "./modules/tooltip.js";
 import { initDropdownMenu } from "./modules/dropdown-menu.js";
 import { initMenuMobile } from "./modules/menu-mobile.js";
+import { initAnimaNumeros } from "./modules/anima-numeros.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -15,3 +16,4 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
+initAnimaNumeros();
