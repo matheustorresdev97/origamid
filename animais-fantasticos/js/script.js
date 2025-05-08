8b8a6ca1749121f91b6d50d8052572bd05612d3c
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-08 18:50:19 -0300

    feat: add modal de login

--- animais-fantasticos/js/script.js
+++ animais-fantasticos/js/script.js
@@ -1,9 +1,11 @@
-import { initAccordion } from "./modules/accordion.js";
-import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
 import { initScrollSuave } from "./modules/scroll-suave.js";
+import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
+import { initAccordion } from "./modules/accordion.js";
 import { initTabNav } from "./modules/tabnav.js";
+import { initModal } from "./modules/modal.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
