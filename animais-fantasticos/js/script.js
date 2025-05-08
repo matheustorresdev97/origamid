b7d294a012d8e1511c58dfb8bced7ae70bd9e8b2
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-08 19:04:31 -0300

    feat: menu mobile

--- animais-fantasticos/js/script.js
+++ animais-fantasticos/js/script.js
@@ -3,8 +3,9 @@ import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
 import { initAccordion } from "./modules/accordion.js";
 import { initTabNav } from "./modules/tabnav.js";
 import { initModal } from "./modules/modal.js";
-import { initTooltip } from "./modules/tooltip.js";
+import { initTooltip } from "./modules/tolltip.js";
 import { initDropdownMenu } from "./modules/dropdown-menu.js";
+import { initMenuMobile } from "./modules/menu-mobile.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
