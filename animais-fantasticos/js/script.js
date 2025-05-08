c01e01e91f5c5ccdeecd6bc5345e82fdcb6c0453
matheustorresdev97 <matheustorresdev@gmail.com>
2025-05-08 20:08:46 -0300

    refactor: fetchBitcoin

--- animais-fantasticos/js/script.js
+++ animais-fantasticos/js/script.js
@@ -4,6 +4,8 @@ import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import fetchAnimais from "./modules/fetch-animais.js";
+import fetchBitcoin from "./modules/fetch-bitcoin.js";
+
 
 import { initDropdownMenu } from "./modules/dropdown-menu.js";
 import { initMenuMobile } from "./modules/menu-mobile.js";
@@ -46,3 +48,4 @@ initFetchBitcoin();
 initAnimacaoScroll();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
